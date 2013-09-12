1ba1c49f9f3af4a7b743df96cb33b7c805a92649
Joe VanLeeuwen <jvnlwn@gmail.com>
2013-09-12 17:17:25 -0400

    added 5 specs. All passing now. Should be good specs.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,71 +1,112 @@
 Parse.initialize("ltHC6n4UFCaXv6WtrRIh2EqlUUtos2cJLleHs1r2", "27H9dOOERvmIJvN0KwLBvPAQ5FaCrwGZNX1YgS0S");
 
-// Create the object.
-var Participant = Parse.Object.extend("Participant");
+// Create the constructor.
+var ToDoClass = Parse.Object.extend("ToDo");
 
-// A Collection containing all instances of TestObject.
-var ParticipantCollection = Parse.Collection.extend({
-  model: Participant
+// A Collection containing all instances of ToDoClass that have been completed.
+var UncompletedToDoCollectionClass = Parse.Collection.extend({
+    model: ToDoClass,
+    query: (new Parse.Query(ToDoClass)).equalTo('completed', false)
 });
 
-var collection = new ParticipantCollection();
+// A Collection containing all instances of ToDoClass that have not been completed.
+var CompletedToDoCollectionClass = Parse.Collection.extend({
+    model: ToDoClass,
+    query: (new Parse.Query(ToDoClass)).equalTo('completed', true)
+});
 
-// collection.fetch({
-//   success: function(collection) {
-//     collection.each(function(object) {
-//     	var thing = $('<div>' + object.get('name') + ', status: ' + object.get('done') + '<div>')
-//     	thing.click(function() {
-//     		object.set('done', true);
-//     		object.save();
-//     	});
-//       $('.container').append(thing)
-//     });
-//   },
-//   error: function(collection, error) {
-//   }
-// });
+var uncompletedToDos = new UncompletedToDoCollectionClass();
+var completedToDos = new CompletedToDoCollectionClass();
 
-
-
-
-var Participant = Parse.Object.extend('Participant',
-	{
-		setProperties: function(name, email, occupation, location) {
-			this.set('name', name);
-			this.set('email', email);
-			this.set('occupation', occupation);
-			this.set('location', location);
-		}
-	}
-);
+var newToDoAdded = false;
+var oldToDoCompleted = true;
 
 $('document').ready(function() {
 
-	$('.save').click(function() {
-		var participant = new Participant();
-		participant.setProperties($('.name').val(),$('.email').val(),$('.occupation').val(),$('.location').val());
+	fetchAndDisplayCollection(uncompletedToDos)
 
-		if(validate()) {
-			participant.save(null, {		
-				success: function(participant) {
-					$('.success').animate({right: "-100px"}, 500);
-				},
-				error: function(participant, error) {
-					$('.failure').animate({right: "-100px"}, 500);
-				}
-			});
+	$('.uncompleted').click(function() {
+		toggleButtons()
+		if (newToDoAdded) {
+			fetchAndDisplayCollection(uncompletedToDos)
 		}
-		else {$('.failure').animate({right: "-100px"}, 500);}
+		else {displayCollection(uncompletedToDos)}
+	})
+
+	$('.completed').click(function() {
+		toggleButtons()
+		if (oldToDoCompleted) {
+			fetchAndDisplayCollection(completedToDos)
+		}
+		else {displayCollection(completedToDos)}
+	})
+
+	$('.save').click(function() {
+		var toDo = new ToDoClass();
+		toDo.set('title', $('.add').val());
+		toDo.set('completed', false);
+		toDo.save({
+			success: function(result) {
+				newToDoAdded = true;
+				addToDoButton();
+				if ($('.uncompleted').hasClass('toggle-button-active')) {
+					addToDo(result);
+				};
+			},
+			error: function(results, error) {
+				console.log(error.description)
+			}
+		});
+	});
+
+
+	$('.add-to-do').click(function() {
+		addToDoButton();
 	});
 });
 
-$('.add-to-do').click(function() {
-		$('.new-to-do').toggleClass('new-to-do-active')
-		$('.thin-line').toggleClass('thin-line-active')
-		$('.container').toggleClass('container-active')
+function addToDo(toDo) {
+	var item = $('<li>' + toDo.get('title') + '</li><hr>');
+	$('.content ul').append(item);
+	return item;
+}
 
+function fetchAndDisplayCollection(collection) {
+	collection.fetch({
+		success: function(resultinCollection) {
+			// clearing out the ul
+			$('.content ul').html('');
+			// this will diplay all toDo's in the specified collection
+			// and attatch a click event to each <li>.
+			resultinCollection.each(function(toDo) {
+				$(addToDo(toDo)).click(function() {
+					console.log(toDo.get('title'));
+				})
+			}) 
+		},
+		error: function(resultinCollection, error) {
+			console.log(error.description);
+		}
 	});
+}
 
+function displayCollection(collection) {
+	$('.content ul').html('');
+	collection.each(function(toDo) {
+		addToDo(toDo)
+	}) 
+}
+
+function toggleButtons() {
+	$('.uncompleted').toggleClass('toggle-button-active')
+	$('.completed').toggleClass('toggle-button-active')
+}
+
+function addToDoButton() {
+	$('.new-to-do').toggleClass('new-to-do-active')
+	$('.thin-line').toggleClass('thin-line-active')
+	$('.container').toggleClass('container-active')
+};
 
 function validate() {
 	inputs = $('input');
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,97 @@
 /*global describe, it */
 'use strict';
 (function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+    describe('ToDo', function() {
+    	this.timeout(15000);
+    	it('when save button is clicked, should save new ToDo to Parse', function(done) {
+    		var result;
 
-            });
+    		var newToDo = "Do this " + Math.floor(Math.random()*100000) + " times."
+            $('.add').val(newToDo)
+
+    		$('.save').click();
+
+    		setTimeout(function(){
+    			var query = new Parse.Query(ToDoClass)
+    			query.equalTo('title', newToDo)
+    			query.find({
+    				success: function(results) {
+                        result = results[0];
+    					expect(result.get('title')).to.equal(newToDo);
+    					done();
+    				},
+    				error: function(result, error) {
+    					done(error.description);
+    				}
+    			});
+    		},2000)
+    	});
+
+        it('when save button is clicked and ToDo button is depressed, should add the new ToDo to the .content ul', function(done) {
+
+            var newToDo = "Do this " + Math.floor(Math.random()*100000) + " times."
+            $('.add').val(newToDo)
+
+            $('.save').click();
+
+            setTimeout(function(){
+                expect($('.content ul li').last().text()).to.equal(newToDo)
+                done();
+            },2000)
         });
-    });
+
+        it('when save button is clicked and ToDo button is NOT depressed, should NOT add the new ToDo to the .content ul', function(done) {
+
+            $('.uncompleted').removeClass('toggle-button-active');
+            var newToDo = "Do this " + Math.floor(Math.random()*100000) + " times."
+            $('.add').val(newToDo)
+
+            $('.save').click();
+
+            setTimeout(function(){
+                expect($('.content ul li').last().text()).to.not.equal(newToDo)
+                done();
+            },2000)
+        });
+
+        it('when ToDo button is depressed, the .content ul should only contain toDos who\'s completed property is false', function(done) {
+            var uncompleted = 0;
+            fetchAndDisplayCollection(uncompletedToDos);
+
+            setTimeout(function(){
+                var allToDos = $('.content ul li');
+                uncompletedToDos.each(function(toDo, index) {
+                    var title = $(allToDos[index]).text()
+                    
+                    if (toDo.get('title') === title) {
+                        if (toDo.get('completed') === false)
+                        uncompleted += 1;
+                    }
+                })
+                expect(uncompleted).to.equal(uncompletedToDos.length)
+                done();
+            },2000)
+        });
+
+        it('when Completed button is depressed, the .content ul should only contain toDos who\'s completed property is true', function(done) {
+            toggleButtons();
+
+            var completed = 0;
+            fetchAndDisplayCollection(completedToDos);
+
+            setTimeout(function(){
+                var allToDos = $('.content ul li');
+                completedToDos.each(function(toDo, index) {
+                    var title = $(allToDos[index]).text()
+                    
+                    if (toDo.get('title') === title) {
+                        if (toDo.get('completed') === true)
+                        completed += 1;
+                    }
+                })
+                expect(completed).to.equal(completedToDos.length)
+                done();
+            },2000)
+        });
+    })
 })();
